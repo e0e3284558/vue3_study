5f4785a235d78ea674b5f3909711a5735403e3c4
e0e3284558 <bifei0827@vip.qq.com>
2021-03-10 22:54:51 +0800

    配置登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Login",
+    component:   import( "../views/account/Login.vue")
   },
   {
     path: "/about",
@@ -14,7 +13,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import( "../views/About.vue")
+  },
+  {
+    path: "/news",
+    name: "News",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( "../views/News.vue")
   }
 ];
 
---
+++ src/views/News.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h1>新闻</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "News"
+    }
+</script>
+
+<style scoped>
+
+</style>
